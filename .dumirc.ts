31c09b34fbc47725f8157a6a61997492e7e2756a
liuyuqin <414097512@qq.com>
2024-05-10 14:02:04 +0800

    build: 修改dumi配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  base: '/front-library',
-  publicPath: '/front-library/',
+  base: '/front-doc',
+  publicPath: '/front-doc/',
   themeConfig: {
     logo: false,
     name: '',
